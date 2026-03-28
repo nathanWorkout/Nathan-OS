13be47f58b137f4b4d519e9d17cea9523fdc3e54
nathanWorkout <delahayenathan313@gmail.com>
2026-03-28 22:01:35 +0100

    header de pmm fait

---
+++ kernel/memory/pmm.h
@@ -0,0 +1,12 @@
+#ifndef PMM_H
+#define PMM_H
+#include <stdint.h>
+
+#define PAGE_SIZE 4096 // Une page fait 4ko -> 1024 * 4
+#define PAGE_SHIFT 5   // Entrée
+
+void pmm_init(uint32_t memory_map_addr, uint32_t region_count);
+void pmm_free_page(uint32_t addr);
+uint32_t pmm_alloc_page(); // Un type qui retourne 32 bits car on a besoin de retourner une adresse 32 bits
+
+#endif
