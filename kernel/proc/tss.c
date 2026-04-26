bca7840f4ebeb395a8212a5c12dc6f0b008b8288
nathanWorkout <delahayenathan313@.com>
2026-04-26 20:53:28 +0200

    petite modif du tss

--- kernel/proc/tss.c
+++ kernel/proc/tss.c
@@ -9,6 +9,8 @@ void tss_init() {
   tss = (tss_t) {0};
   tss.ss0 = 0x10;  
   gdt_set_tss_entry((uint32_t)&tss, sizeof(tss) - 1);
+  tss.io_map_base = sizeof(tss);
+  __asm__ volatile ("ltr %%ax" : : "a"(0x28));
 
   serial_print_hex((uint32_t)&tss);
   serial_print("\n");
@@ -16,5 +18,6 @@ void tss_init() {
   serial_print("\n");
   serial_print_hex(tss.ss0);
   serial_print("\n");
+  serial_print_hex(tss.io_map_base);
 }
 
